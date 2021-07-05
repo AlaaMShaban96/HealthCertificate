03706f1a7dac8ba885fbd93736808d7a5ffbc96a
alaa mohamed <ala96ala96@gmail.com>
2021-07-05 18:31:36 +0200

    add test seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         $this->call(IdentityTypeSeed::class);
         $this->call(NationalitySeed::class);
-        $this->call(IdentityTypeSeed::class);
+        $this->call(TestTypeSeed::class);
 
     }
 }
